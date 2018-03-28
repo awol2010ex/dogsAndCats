7c5565917121544549a7784fb9225fc316f52f18
awol2010ex <awol2010ex@gmail.com>
2018-03-28 16:56:06 +0800

    pytorch练习 dogs and cats 分类: 增加tensorboard

--- train_cnn.py
+++ train_cnn.py
@@ -8,6 +8,8 @@ import torchvision
 from model_cnn import CNN
 from torch.utils.data.sampler import SubsetRandomSampler
 import random
+import os
+from tensorboardX import SummaryWriter
 #数据量每次随机10%取样
 def SplitSampler(datalen, split=0.1):
     idx = list(range(datalen))
@@ -19,6 +21,7 @@ def SplitSampler(datalen, split=0.1):
 
 
 if __name__ == '__main__':
+    writer = SummaryWriter()
     torch.manual_seed(1)
 
     EPOCH = 1000
@@ -39,8 +42,12 @@ if __name__ == '__main__':
     print(test_data.class_to_idx)
     # 将 Dataset 封装为 DataLoader
     test_data_loader = Data.DataLoader(dataset=test_data, shuffle=True, batch_size=1)
-    
-    cnn = CNN()
+
+    #加载已训练模型（或中断）
+    if os.path.exists("model/model.pkl"):
+        cnn= torch.load("model/model.pkl")
+    else:
+        cnn = CNN()
     print(cnn)
     
     #optimizer
@@ -64,8 +71,10 @@ if __name__ == '__main__':
     # img.show()
 
     #training loop
+    step=0
     for epoch in range(EPOCH):
         train, devel = SplitSampler(len(training_data))
+        #取10%采样
         training_data_loader = Data.DataLoader(dataset=training_data, shuffle=False, batch_size=BATCH_SIZE, num_workers=10, sampler=devel)
 
         i=0
@@ -91,15 +100,37 @@ if __name__ == '__main__':
             #优化器参数更新
             optimizer.step()
             print('Epoch[{}/{}/{}], loss: {:.6f}'.format(epoch + 1, EPOCH, (i+1)*BATCH_SIZE,loss.data[0]))
+            # 写loss指标进图表
+            writer.add_scalar('loss', loss.data[0], step)
             i=i+1
+            step=step+1
         
         print('Epoch[{}/{}], loss: {:.6f}'.format(epoch + 1, EPOCH, loss.data[0]))
+
+
+        for name, param in cnn.named_parameters():
+            writer.add_histogram(name, param.clone().cpu().data.numpy(), epoch)
+
+
+        #触发dropout防止过拟合
         cnn.eval()
+
+
+
+        #使用测试训练集查看该轮训练情况
         for i, (x, y) in enumerate(test_data_loader):
             batch_x = Variable(x)
             batch_y = Variable(y)
             # 输入训练数据
             output = cnn(batch_x)
+
+
+
             print('fact={},predict={}'.format(batch_y.data[0],torch.max(output, 1)[1].data.numpy().squeeze()))
-    torch.save(cnn, 'model/model.pkl')
+        # 保存训练图
+
+        torch.onnx.export(cnn, batch_x, "model/model.proto", verbose=True)
+        writer.add_graph_onnx("model/model.proto")
+
+        torch.save(cnn, 'model/model.pkl')
          
